a36f2eaae5b3b95aa3243a89db3d057ce7d24f90
Duncan <marjoribanks.d@gmail.com>
2018-05-03 11:23:39 +0100

    added seed data to seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+require_relative('../models/student')
+require('pry')
+
+student1 = Student.new({
+  "first_name" => "Hermiony",
+  "second_name" => "Granger",
+  "house" => "Griffindor",
+  "age" => 15
+  })
+student1.save
+
+student2 = Student.new({
+  "first_name" => "Ron",
+  "second_name" => "Weasley",
+  "house" => "Griffindor",
+  "age" => 14
+  })
+student2.save
+
+student3 = Student.new({
+  "first_name" => "Draco",
+  "second_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => 15
+  })
+student3.save
+
+
+binding.pry
+nil
